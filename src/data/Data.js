1c08866768e6ed7cb1c6fa45a10a7126dcf75010
RishiRagu2510 <rishiragu25@gmail.com>
2024-06-27 09:07:07 +0530

    first commit

---
+++ src/data/Data.js
@@ -0,0 +1,92 @@
+import  Dosa from "../images/dosa.jpg";
+import Idly from "../images/idly.jpg";
+import Poori from "../images/poori.jpg";
+import Paratto from "../images/paratto.jpg";
+import Chappathi from "../images/chappathi.jpg";
+import Pongal from "../images/pongal.jpg";
+export const MenuList = [
+  {
+    name: "Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Dosa,
+    price: "₹40",
+  },
+  {
+    name: "Chappathi",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Chappathi,
+    price: "₹40",
+  },
+  {
+    name: "Paratto",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Paratto,
+    price: "₹40",
+  },
+  {
+    name: "Pongal",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Pongal,
+    price: "₹50",
+  },
+  {
+    name: "Idly",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Idly,
+    price: "₹20",
+  },
+  {
+    name: "Poori",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Poori,
+    price: "₹30",
+  },
+  {
+    name: "Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Dosa,
+    price: "₹40",
+  },
+  {
+    name: "Chappathi",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Chappathi,
+    price: "₹40",
+  },
+  {
+    name: "Paratto",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Paratto,
+    price: "₹40",
+  },
+  {
+    name: "Pongal",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Pongal,
+    price: "₹50",
+  },
+  {
+    name: "Idly",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Idly,
+    price: "₹20",
+  },
+  {
+    name: "Poori",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Poori,
+    price: "₹30",
+  },
+]
