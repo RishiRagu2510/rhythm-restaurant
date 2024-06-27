bbaeb6ea3c6e745c0576661645188458f57e048c
RishiRagu2510 <rishiragu25@gmail.com>
2024-06-27 21:23:31 +0530

    Describe your changes

--- src/data/Data.js
+++ src/data/Data.js
@@ -4,89 +4,102 @@ import Poori from "../images/poori.jpg";
 import Paratto from "../images/paratto.jpg";
 import Chappathi from "../images/chappathi.jpg";
 import Pongal from "../images/pongal.jpg";
+import Biryani from "../images/biriyani.jpg";
+import CM from "../images/chicken manchurian.jpg";
+import CN from "../images/chicken noodles.jpg";
+import CR from "../images/chicken rice.jpg";
+import EB from "../images/egg biryani.jpg";
+import EN from "../images/egg noodles.jpg";
+import ER from "../images/egg rice.jpg";
+import Uttappam from "../images/uttappam.jpg";
+import VN from "../images/veg noodles.jpg";
+import VR from "../images/vegrice.jpg";
+
 export const MenuList = [
   {
     name: "Dosa",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
     image: Dosa,
     price: "₹40",
+    favourite:"yes"
   },
   {
     name: "Chappathi",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
     image: Chappathi,
     price: "₹40",
+    favourite:"yes"
   },
   {
     name: "Paratto",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
     image: Paratto,
     price: "₹40",
+    favourite:"yes"
   },
   {
     name: "Pongal",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
     image: Pongal,
     price: "₹50",
   },
   {
     name: "Idly",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
     image: Idly,
     price: "₹20",
   },
   {
     name: "Poori",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
     image: Poori,
     price: "₹30",
+    favourite:"yes"
   },
   {
-    name: "Dosa",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
-    image: Dosa,
+    name: "Biryani",
+    image: Biryani,
+    price: "₹120",
+    favourite:"yes"
+  },
+  {
+    name: "Chicken Manchurian",
+    image: CM,
+    price: "₹90",
+    favourite:"yes"
+  },
+  {
+    name: "Chicken Noodles",
+    image: CN,
+    price: "₹80",
+    favourite:"yes"
+  },
+  {
+    name: "Chicken Rice",
+    image:CR,
+    price: "₹90",
+  },
+  {
+    name: "Egg Biryani",
+    image: EB,
+    price: "₹110",
+  },
+  {
+    name: "Egg Noodles",
+    image: EN,
+    price: "₹90",
+  },
+  {
+    name: "Egg Rice",
+    image: ER,
+    price: "₹80",
+  },
+  {
+    name: "Uttappam",
+    image: Uttappam,
     price: "₹40",
+  },{
+    name: "Veg Noodles",
+    image: VN,
+    price: "₹70",
   },
   {
-    name: "Chappathi",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
-    image: Chappathi,
-    price: "₹40",
-  },
-  {
-    name: "Paratto",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
-    image: Paratto,
-    price: "₹40",
-  },
-  {
-    name: "Pongal",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
-    image: Pongal,
-    price: "₹50",
-  },
-  {
-    name: "Idly",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
-    image: Idly,
-    price: "₹20",
-  },
-  {
-    name: "Poori",
-    description:
-      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
-    image: Poori,
-    price: "₹30",
+    name: "Veg Rice",
+    image: VR,
+    price: "₹70",
   },
 ]
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -9,14 +9,71 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { EffectCoverflow, Pagination, Navigation, Autoplay } from "swiper/modules";
 
 const Menu = () => {
   return (
     <Layout>
-      <Typography variant="h5" sx={{ml:14,mt:5,fontWeight:"bold"}}>SOUTH INDIAN FOOD</Typography>
+      <Box sx={{ width: "80%", margin: "auto", mt: 2, mb: 4 }}>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={3}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          pagination={{ clickable: true }}
+          navigation
+          autoplay={{ delay: 2500, disableOnInteraction: false }}
+          modules={[EffectCoverflow, Pagination, Navigation, Autoplay]}
+        >
+          {MenuList.map((menu, index) => (
+            <SwiperSlide key={index}>
+              <Card>
+                <CardMedia
+                  sx={{ minHeight: "200px" }}
+                  component={"img"}
+                  src={menu.image}
+                  alt={menu.name}
+                />
+                <CardContent>
+                  <Typography variant="h6" sx={{ textAlign: "center" }}>
+                    {menu.name}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
+
+      <Typography
+        variant="h5"
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          mt: 2,
+          mb: 4,
+          fontWeight: "bold",
+        }}
+      >
+        SOUTH INDIAN FOOD
+      </Typography>
+
       <Box sx={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
         {MenuList.map((menu) => (
-          <Card sx={{ maxWidth: "300px", display: "flex", m: 2 }}>
+          <Card sx={{ maxWidth: "300px", display: "flex", m: 2 }} key={menu.name}>
             <CardActionArea>
               <CardMedia
                 sx={{ minHeight: "200px" }}
@@ -24,11 +81,10 @@ const Menu = () => {
                 src={menu.image}
                 alt={menu.name}
               />
-              <CardContent >
-                <Typography variant="h5" gutterBottom component={"div"} sx={{textAlign:"center"}}>
+              <CardContent>
+                <Typography variant="h5" gutterBottom component={"div"} sx={{ textAlign: "center" }}>
                   {menu.name}
                 </Typography>
-                
               </CardContent>
             </CardActionArea>
           </Card>
