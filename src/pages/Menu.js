88d3dc834e492cec757b392b5f8f44804c8b563e
RishiRagu2510 <rishiragu25@gmail.com>
2024-06-29 11:48:28 +0530

    justfied the content

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -89,7 +89,7 @@ const Menu = () => {
           <Card sx={{ maxWidth: "300px", display: "flex", m: 2 }} key={menu.name}>
             <CardActionArea>
               <CardMedia
-                sx={{ minHeight: "150px" }} 
+                sx={{ minHeight: "150px" }} // Adjust the minHeight here
                 component={"img"}
                 src={menu.image}
                 alt={menu.name}
